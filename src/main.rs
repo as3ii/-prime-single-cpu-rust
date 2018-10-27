978192805b7f29cd6b8129da6d638c6c1700388a
Lorenzo Debertolis <as3ii777@gmail.com>
2018-10-27 15:49:46 +0200

    added time measurement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod lib;
 use std::env;
 use std::io::{stdin, stdout, Write};
+use std::time::Instant;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -27,14 +28,22 @@ fn main() -> std::io::Result<()> {
 
     let mut result: Vec<u128> = Vec::new();
 
+    let start_time = Instant::now();
+
     lib::calc_prime(0, max, &mut result);
+
+    println!(
+        "Proces finished in {}s {}ms",
+        start_time.elapsed().as_secs(),
+        start_time.elapsed().subsec_millis()
+    );
+
     print!("Do you want to see te result? [y/n] ");
     stdout().flush()?;
     let mut response = String::new();
     stdin()
         .read_line(&mut response)
         .expect("Failed to read line");
-    println!("{}", response);
     if response == "y\n" {
         lib::print_vec(&result);
     }
